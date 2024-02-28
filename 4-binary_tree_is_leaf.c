b4a8bdd4552b67675ca4675c1aec8eca8963f3f5
Evans Manyala <evans.manyala@gmail.com>
2024-02-28 10:36:26 +0300

    Attempt no: 3. Fixing errors in code

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -5,10 +5,11 @@
  *
  * Return: 1 if node is a leaf, 0 otherwise
  */
+
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
 	if (!node)
 		return (0);
 
-	if (node->left == NULL && node->right == NULL)
+	return (node->left == NULL && node->right == NULL);
 }
