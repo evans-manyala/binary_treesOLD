5551254a3a273846f8eaf7da4740393aa3c35dab
Evans Manyala <evans.manyala@gmail.com>
2024-02-28 23:17:07 +0300

    Betty style error fix

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,20 +1,21 @@
 /**
- * binary_tree_nodes - counts the nodes with at least 1 child in a binary tree
- * @tree: pointer to the root node of the tree to count the number of nodes
+ * binary_tree_nodes - counts the nodes with at least 1 child
+ * in a binary tree no. of nodes
  *
- * Return: number of nodes with at least 1 child in the binary tree, or 0 if tree is NULL.
+ * Return: no of nodes with at least 1 child in the binary tree
+ * or 0 if tree is NULL.
  */
 #include "binary_trees.h"
 
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-    size_t nodes = 0;
+	size_t nodes = 0;
 
-    if (tree == NULL)
-        return (0);
-    if (tree->left != NULL || tree->right != NULL)
-        nodes = 1;
-    nodes += binary_tree_nodes(tree->left);
-    nodes += binary_tree_nodes(tree->right);
-    return (nodes);
+	if (tree == NULL)
+		return (0);
+	if (tree->left != NULL || tree->right != NULL)
+		nodes = 1;
+	nodes += binary_tree_nodes(tree->left);
+	nodes += binary_tree_nodes(tree->right);
+	return (nodes);
 }
