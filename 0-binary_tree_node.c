dc2bdd8be430f2dc0389e9d632c7ef12bccbe9d2
Evans Manyala <evans.manyala@gmail.com>
2024-02-28 08:15:47 +0300

    Adding error fixes

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -9,16 +9,24 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_node;
+	binary_tree_t *new_node;
 
-    new_node = malloc(sizeof(binary_tree_t));
-    if (!new_node)
-        return (NULL);
+	new_node = malloc(sizeof(binary_tree_t));
 
-    new_node->n = value;
-    new_node->parent = parent;
-    new_node->left = NULL;
-    new_node->right = NULL;
+	if (!new_node)
+		return (NULL);
 
-    return (new_node);
+	if (parent->left)
+	{
+		new_node->left = parent->left;
+		parent->left->parent = new_node;
+	}
+
+	parent->left = new_node;
+	new_node->n = value;
+	new_node->parent = parent;
+	new_node->left = NULL;
+	new_node->right = NULL;
+
+	return (new_node);
 }
