998fa390ddc2c464958b55cce08b31fb506aac85
Evans Manyala <evans.manyala@gmail.com>
2024-02-28 15:01:41 +0300

    Adding error fixes

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,15 +1,16 @@
 #include "binary_trees.h"
 /**
- * binary_tree_preorder - Function goes through a binary tree using pre-order traversal
+ * binary_tree_preorder - Function goes through a binary tree
+ * using pre-order traversal
  * @tree: Tree
  *
  * Return: Void
  */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-        if (tree == NULL || func == NULL)
-                return;
-        func(tree->n);
-        binary_tree_preorder(tree->left, func);
-        binary_tree_preorder(tree->right, func);
+	if (tree == NULL || func == NULL)
+		return;
+	func(tree->n);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
 }
