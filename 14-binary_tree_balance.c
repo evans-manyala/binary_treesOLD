975aba7e420d91a1cb731975c11844a82dc7c694
Evans Manyala <evans.manyala@gmail.com>
2024-02-29 00:07:35 +0300

    Adding code logic error fix

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -15,3 +15,19 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 	return (int)(left_height - right_height);
 }
+/**
+ * binary_tree_height - Function measures the height of a binary tree.
+ * @tree: A pointer to the root node of the tree to measure the height.
+ *
+ * Return: If tree is NULL, your function must return 0, else return height.
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (!tree)
+		return 0;
+
+	size_t left_height = binary_tree_height(tree->left);
+	size_t right_height = binary_tree_height(tree->right);
+
+	return (1 + (left_height > right_height ? left_height : right_height));
+}
