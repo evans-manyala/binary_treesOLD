83d1851a6ae2c5187d692652a0426a05b2d873ac
Evans Manyala <evans.manyala@gmail.com>
2024-02-29 17:07:51 +0300

    Adding final code fix

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -25,7 +25,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 			return (1);
 
 		return (binary_tree_is_perfect(tree->left) &&
-			binary_tree_is_perfect(tree->right));
+				binary_tree_is_perfect(tree->right));
 	}
 
 	return (0);
