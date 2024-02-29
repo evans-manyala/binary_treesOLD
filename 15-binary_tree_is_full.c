64a41dc201f12c65379f2cf851d20fdf64594bf8
Evans Manyala <evans.manyala@gmail.com>
2024-02-29 07:48:01 +0300

    Adding code logic

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,13 +1,15 @@
+#include "binary_trees.h"
 /**
  * binary_tree_is_full - Function checks if binary tree is full
  *
  */
-binary_tree_is_full(const binary_tree_t *node)
+int binary_tree_is_full(const binary_tree_t *node)
 {
-    if (node == NULL)
-        return (0);
+	if (node == NULL)
+		return (0);
 
-    if (node->left == NULL && node->right == NULL)
-        return (1);
-    else if (node->left != NULL && node->right != NULL)
-        return (binary_tree_is_full(node->left) && binary_tree_is_full(node->right));
+	if (node->left == NULL && node->right == NULL)
+		return (1);
+	else if (node->left != NULL && node->right != NULL)
+		return (binary_tree_is_full(node->left) && binary_tree_is_full(node->right));
+}
