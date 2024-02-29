e22059d7311283382a70cf2674e1cebacab5b478
Evans Manyala <evans.manyala@gmail.com>
2024-02-29 08:05:37 +0300

    Adding Betty Style error fixx

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,7 +1,8 @@
 #include "binary_trees.h"
 /**
  * binary_tree_is_full - Function checks if binary tree is full
- *
+ * @node: Pointer to the root node of the tree to check
+ * Return: 1 if full, 0 otherwise. If node is NULL, return 0.
  */
 int binary_tree_is_full(const binary_tree_t *node)
 {
@@ -12,4 +13,5 @@ int binary_tree_is_full(const binary_tree_t *node)
 		return (1);
 	else if (node->left != NULL && node->right != NULL)
 		return (binary_tree_is_full(node->left) && binary_tree_is_full(node->right));
+	return (0);
 }
